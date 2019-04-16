355847621045ad274f172ac6f70cec9cd2142e2a
will-r-warner <47200097+will-r-warner@users.noreply.github.com>
2019-04-16 15:23:05 -0500

    Updated Contact Form
    Worked on styling, functionality, user feedback

--- email.js
+++ email.js
@@ -1,23 +1,38 @@
-contactBtn = document.getElementById("js-contact__button");
-contactBtn.addEventListener("onclick", sendEmail);
+var contactBtn = document.getElementById("js-contact__submit");
+contactBtn.addEventListener("click", sendEmail);
+
+var resetBtn = document.getElementById("js-contact__reset");
+resetBtn.addEventListener("click", clearContactForm);
 
 function sendEmail(){
 
-  contactName = document.getElementById("js-contact__name").textContent;
-  contactEmail = document.getElementById("js-contact__email").textContent;
-  contactMessage = document.getElementById("js-contact__message").textContent;
-
-  document.getElementById("js-contact__name").textContent = "";
-  document.getElementById("js-contact__email").textContent = "";
-  document.getElementById("js-contact__message").textContent = "";
-  document.getElementById("js-contact__button").textContent = "";
+  var contactName = document.getElementById("js-contact__name").textContent;
+  var contactEmail = document.getElementById("js-contact__email").textContent;
+  var contactMessage = document.getElementById("js-contact__message").value;
+	
+  clearContactForm();
 
   Email.send({
     SecureToken : "d3b95c08-53cb-417b-9786-e652757e1724",
     To : 'will.r.warner@gmail.com',
-    From : contactEmail,
-    Subject : "Contact from WillRWarner.com",
-    Body : contactMessage
-  }).then(message => alert(message));
+    From : 'donotreply@willrwarner.com',
+    Subject : "Contact from willrwarner.com",
+    Body : contactMessage + contactEmail
+  }).then(message => updateContactStatus(message));
+}
 
+function updateContactStatus(message) {
+  if (message === "OK"){
+	document.getElementById("js-contact__status").style.display = "Block";
+  	document.getElementById("js-contact__status").textContent = "Thank you for contacting me. Your message has been submitted successfully!";
+  }else{
+	document.getElementById("js-contact__status").style.display = "Block";
+	document.getElementById("js-contact__status").textContent = "I'm sorry but there was an error submitting your message. Please send me an email at will.r.warner@gmail.com";
+  }
+}
+
+function clearContactForm() {
+  document.getElementById("js-contact__name").value = "";
+  document.getElementById("js-contact__email").value = "";
+  document.getElementById("js-contact__message").value = "";
 }
