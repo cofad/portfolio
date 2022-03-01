e59aaa548bb9b06dc414059ecbea8437a9f48c07
Will Warner <will.r.warner@gmail.com>
2022-03-01 12:52:19 +0000

    remove typewriter effect

--- index.js
+++
@@ -1,6 +0,0 @@
-import * as typeWrite from "./type-write.js";
-
-const typeWriteable = typeWrite.getTypeWriteableElement();
-const typeWritten = typeWrite.getTypeWrittenElement();
-
-typeWrite.typeWriteElement(typeWritten, typeWriteable);
