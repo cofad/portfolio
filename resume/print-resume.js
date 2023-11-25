f5954197949f0c914215433bfb5fb0fa398d6743
Will Warner <47200097+cofad@users.noreply.github.com>
2023-11-25 20:13:01 +0000

    chore: migrate resume to this repo

---
+++ resume/print-resume.js
@@ -0,0 +1,21 @@
+import { launch } from "puppeteer";
+import fs from "node:fs";
+
+(async () => {
+  const packageJson = JSON.parse(fs.readFileSync("./package.json").toString());
+
+  // Only use this config for trusted code
+  const browser = await launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: 'new',
+  });
+
+  const page = await browser.newPage();
+  await page.goto(`file://${process.cwd()}/src/resume.html`);
+  await page.pdf({
+    path: `resume-${packageJson.version}.pdf`,
+    format: "letter",
+  });
+
+  await browser.close();
+})();
