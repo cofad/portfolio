2c16b6b67dce05cbfbfa09395e191ddf31945b56
Will Warner <47200097+cofad@users.noreply.github.com>
2023-11-25 03:56:01 +0000

    chore: migrate project to angular

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# willrwarner.com
+# willrwarner.dev
 
 My personal website to demonstrate my web development skills and provide some background information about me.
 
---
+++ front-end/src/app/pages/resume/resume.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-resume',
+  templateUrl: './resume.component.html',
+  styleUrl: './resume.component.scss'
+})
+export class ResumeComponent {
+
+}
