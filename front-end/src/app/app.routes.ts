45333eaf2fbca8c8c4ebe8bd60b344487d3a7ac3
Will Warner <47200097+cofad@users.noreply.github.com>
2023-11-27 02:27:20 +0000

    feat: combine pages into home

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { ResumeComponent } from './pages/resume/resume.component';
 
 export const routes: Routes = [
@@ -9,16 +7,6 @@ export const routes: Routes = [
         path: '',
         component: HomeComponent,
         title: 'Home',
-    },
-    {
-        path: 'about',
-        component: AboutComponent,
-        title: 'About',
-    },
-    {
-        path: 'contact',
-        component: ContactComponent,
-        title: 'Contact',
     },
     {
         path: 'resume',
