4fd19b1031686715291bc1c48c4679b362f3f268
Will Warner <47200097+cofad@users.noreply.github.com>
2025-01-13 03:17:25 +0000

    feat: remove resume route

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ResumeComponent } from './pages/resume/resume.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 export const routes: Routes = [
