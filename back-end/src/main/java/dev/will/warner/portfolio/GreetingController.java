49af10056e7f99bc2858f88e095f2c010e597843
William Warner <will.warner@nerdery.com>
2025-11-23 08:08:34 -0600

    feat: add initial back-end

---
+++ back-end/src/main/java/dev/will/warner/portfolio/GreetingController.java
@@ -0,0 +1,13 @@
+package dev.will.warner.portfolio;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello from the portfolio backend!";
+    }
+}
