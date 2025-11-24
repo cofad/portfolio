ec38d3ab39e7b401d825d7ef75253e8f08956d15
William Warner <will.warner@nerdery.com>
2025-11-24 15:59:54 -0600

    feat: allow CORs for front-end

---
+++ back-end/src/main/java/dev/will/warner/portfolio/config/WebConfig.java
@@ -0,0 +1,19 @@
+package dev.will.warner.portfolio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") 
+            .allowedOrigins("localhost:5500", "will-warner.dev")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            // .allowedHeaders("*")
+            // .allowCredentials(true) // Allow credentials (e.g., cookies, authorization headers)
+            .maxAge(3600); // Pre-flight cache duration (in seconds)
+    }
+}
